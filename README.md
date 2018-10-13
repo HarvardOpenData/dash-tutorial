71e34e2f1e7c13f7695ead9c5974e99ccd12bd81
Emma Ling <emmaling@Emmas-MacBook-Pro-39.local>
2018-10-13 09:48:02 -0400

    added table with housing data

--- README.md
+++ README.md
@@ -10,5 +10,6 @@ pip install dash-core-components==0.33.0  # Supercharged components
 Run `python app.py` in your terminal.
 ## Bootcamp
 1. Import the data using pandas.
+2. Display a table with the data
 2. Change the title of the page to something more appropriate.
 3. Link dash and plotly to their respective websites.
--- app.py
+++ app.py
@@ -1,16 +1,40 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.read_csv("house_rankings_2018.csv")
 
 app.layout = html.Div(children=[
     html.H1(children='Dash Bootcamp'),
     html.Div(children='''
         Learn how to build web apps using dash and plotly
     '''),
+    html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in df.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(df.iloc[i][col]) for col in df.columns
+        ]) for i in range(len(df))]
+    ),
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
 ])
 
 if __name__ == '__main__':
