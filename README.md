e415931aacaeae20071b134c6e33845dd7ec6b86
Emma Ling <emmaling@Emmas-MacBook-Pro-39.local>
2018-10-13 12:49:04 -0400

    dropdowns don't work :(

--- README.md
+++ README.md
@@ -5,11 +5,14 @@ Learn how to build web apps using [dash](https://plot.ly/products/dash/) and [pl
 pip install dash==0.28.2  # The core dash backend
 pip install dash-html-components==0.13.2  # HTML components
 pip install dash-core-components==0.33.0  # Supercharged components
+pip install plotly==3.3.0
 ```
 ## Running the app
 Run `python app.py` in your terminal.
 ## Bootcamp
 1. Import the data using pandas.
 2. Display a table with the data
+2. Create a bar graph showing the number of people who ranked each house first.
+2. Create an interactive bar chart with a dropdown menu that allows you to select a house and view the distribution of rankings for that house.
 2. Change the title of the page to something more appropriate.
 3. Link dash and plotly to their respective websites.
--- app.py
+++ app.py
@@ -2,12 +2,24 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+import plotly.graph_objs as go
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = pd.read_csv("house_rankings_2018.csv")
+df.set_index("House", inplace=True)
+
+ranked_first_bar_chart = go.Figure(
+    data=[go.Bar(
+        x=df.index,
+        y=df.iloc[:,0]
+    )],
+    layout=go.Layout(
+        title='Houses Ranked 1st in 2018'
+    )
+)
 
 app.layout = html.Div(children=[
     html.H1(children='Dash Bootcamp'),
@@ -24,18 +36,48 @@ app.layout = html.Div(children=[
         ]) for i in range(len(df))]
     ),
     dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
+        id='ranked-first',
+        figure=ranked_first_bar_chart
+    ),
+    dcc.Dropdown(
+    id='my-dropdown',
+    options=[
+        {'label': 'New York City', 'value': 'NYC'},
+        {'label': 'Montreal', 'value': 'MTL'},
+        {'label': 'San Francisco', 'value': 'SF'}
+    ],
+    value='NYC'
+    ),
+    html.Div(id='output-container'),
+
+    dcc.Dropdown(
+        id='house-dropdown',
+        options=[{'label': house, 'value': house} for house in df.index],
+        multi=True
+    ),
+    html.Div(
+        id='house-rankings'
     )
 ])
+@app.callback(
+    dash.dependencies.Output('output-container', 'children'),
+    [dash.dependencies.Input('my-dropdown', 'value')])
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
+@app.callback(
+    dash.dependencies.Output('house-rankings', 'children'),
+    [dash.dependencies.Input('house-dropdown', 'value')])
+def house_rankings(house):
+    return go.Figure(
+        # data=[go.Bar(
+        #     x=df.columns,
+        #     y=df.iloc[house,:]
+        # )]
+        data=[go.Bar(
+            x=df.index,
+            y=df.iloc[:,0]
+        )]
+    )
 
 if __name__ == '__main__':
     app.run_server(debug=True)
